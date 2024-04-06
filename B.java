fed9f5755448d3815c264d153108685cd41d227c
João Paulo Chaves Gonçalves Elias <joao.elias2@estudante.ufla.br>
2024-04-06 17:22:02 -0300

    Alterações feitas por João Paulo

---
+++ B.java
@@ -0,0 +1,33 @@
+public class B {
+    private int B1;
+    private float B2;
+
+    public B(int B1, float B2) {
+        this.B1 = B1;
+        this.B2 = B2;
+    }
+
+    public int getB1() {
+        return B1;
+    }
+
+    public void setB1(int B1) {
+        this.B1 = B1;
+    }
+
+    public float getB2() {
+        return B2;
+    }
+
+    public void setB2(float B2) {
+        this.B2 = B2;
+    }
+
+    public void MB1() {
+        System.out.println("Executando método MB1 de B");
+    }
+
+    public void MB2() {
+        System.out.println("Executando método MB2 de B");
+    }
+} 
